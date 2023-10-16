522d64007e32f088564c1ebb6559544e84884440
Skillfullfiber8 <aniruddhababu@gmail.com>
2023-10-16 16:42:43 +0530

    Aligned images

--- components/Home.js
+++ components/Home.js
@@ -1,11 +1,61 @@
-import { View, Text, Image, Pressable } from "react-native";
+import { View, Text, Image, TouchableOpacity, StyleSheet } from "react-native";
 
 export default function Home (){
     return(
-        <View>
-            <Pressable>
-            <Image source={require('C:\parkpulse\assets\brookfeilds_image.png')} />
-            </Pressable>
+        <View style = {styles.container_img}>
+            <TouchableOpacity>
+                <Image 
+                    source={require('./image/brookfeilds_image.png')}
+                    style={{
+                        height: 130, 
+                        width: 319.36,
+                        justifyContent: 'center'
+                           }}    
+                />
+            </TouchableOpacity>
+
+            <TouchableOpacity>
+                <Image 
+                    source={require('./image/brookfeilds_image.png')}
+                    style={{
+                        height: 130, 
+                        width: 319.36,
+                        justifyContent: 'center'
+                           }}    
+                />
+            </TouchableOpacity>
+
+            <TouchableOpacity>
+                <Image 
+                    source={require('./image/brookfeilds_image.png')}
+                    style={{
+                        height: 130, 
+                        width: 319.36,
+                        justifyContent: 'center'
+                           }}    
+                />
+            </TouchableOpacity>
+
+            <TouchableOpacity>
+                <Image 
+                    source={require('./image/brookfeilds_image.png')}
+                    style={{
+                        height: 130, 
+                        width: 319.36,
+                        justifyContent: 'center',
+                        borderRadius: 8
+                           }}    
+                />
+            </TouchableOpacity>
+            
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container_img:    {
+        flex: 1,
+        justifyContent: 'space-around',
+        alignItems: 'center'
+    }
+    })
