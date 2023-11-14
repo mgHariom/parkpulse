2bd3f47e7663a7c2edccda77b8bdef4999b07399
mgHariom <mghariom17@gmail.com>
2023-11-14 21:31:47 +0530

    installed firebase sdk

---
+++ firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBtyCYk90iF0grEl60AAPNM-G3kKnKIlh8",
+  authDomain: "parkpulse-b6150.firebaseapp.com",
+  projectId: "parkpulse-b6150",
+  storageBucket: "parkpulse-b6150.appspot.com",
+  messagingSenderId: "359422083315",
+  appId: "1:359422083315:web:cdfeb66efe98bb61ed3786",
+  measurementId: "G-F6E2CYGSYE"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
