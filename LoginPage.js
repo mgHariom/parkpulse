169ef41464dedabbe80f0d7c22af80268a5ad99d
mgHariom <mghariom17@gmail.com>
2023-11-17 10:48:44 +0530

    koguin error resolved

--- LoginPage.js
+++ LoginPage.js
@@ -1,20 +1,19 @@
-import React, {useRef, useState} from 'react'
-import { Text, View,TouchableOpacity, TextInput, Alert } from 'react-native'
-//import {firebaseRecaptchaVerifierModal} from 'expo-firebse-recaptcha'
-import firebaseConfig from 'firebase'
-import firebase from 'firebase/compat'
+import React, { useRef, useState } from 'react';
+import { Text, View, TouchableOpacity, TextInput, Alert, StyleSheet } from 'react-native';
+import firebase from 'firebase/app';
+import 'firebase/auth';
 
 export default LoginPage = () => {
     const [phoneNumber, setPhoneNumber] = useState('');
     const [code, setCode] = useState('');
-    const [verificationId, setVerficationId] = useState(null);
-    
+    const [verificationId, setVerificationId] = useState(null);
+
     const sendVerification = () => {
         const phoneProvider = new firebase.auth.PhoneAuthProvider();
         phoneProvider
             .verifyPhoneNumber(phoneNumber)
-            .then(setVerficationId)
-            setPhoneNumber('');
+            .then(setVerificationId);
+        setPhoneNumber('');
     };
 
     const confirmCode = () => {
@@ -23,29 +22,29 @@ export default LoginPage = () => {
             code
         );
         firebase.auth().signInWithCredential(credential)
-        .then(() => {
-            setCode('')
-        })
-        .catch((error) => {
-            //alert message
-            alert(error)
-        })
-        Alert.alert('Login Successful')
+            .then(() => {
+                setCode('');
+                Alert.alert('Login Successful');
+            })
+            .catch((error) => {
+                // alert message
+                alert(error);
+            });
     };
 
-    return(
-        <View style={StyleSheet.container}>
-            <Text style={StyleSheet.otpText}>
+    return (
+        <View style={styles.container}>
+            <Text style={styles.otpText}>
                 Login using otp
             </Text>
             <TextInput
-                placeholder='Phone NUmber with Country Code'
+                placeholder='Phone Number with Country Code'
                 onChangeText={setPhoneNumber}
                 keyboardType='phone-pad'
                 autoCompleteType='tel'
-                style={styles.TextInput}
+                style={styles.textInput}
             />
-            <TouchableOpacity style={styles.sendVeerification} onPress={sendVerification}>
+            <TouchableOpacity style={styles.sendVerification} onPress={sendVerification}>
                 <Text style={styles.buttonText}>
                     Send Verification
                 </Text>
@@ -53,66 +52,53 @@ export default LoginPage = () => {
 
             <TextInput
                 placeholder='Confirm Code'
-                onChangeText={setPhoneNumber}
+                onChangeText={setCode}
                 keyboardType='number-pad'
                 autoCompleteType='tel'
-                style={styles.TextInput}
+                style={styles.textInput}
             />
-            <TouchableOpacity style={styles.sendVeerification} onPress={confirmCode}>
+            <TouchableOpacity style={styles.sendVerification} onPress={confirmCode}>
                 <Text style={styles.buttonText}>
                     Confirm Verification
                 </Text>
             </TouchableOpacity>
         </View>
     );
-}
+};
 
-const styles = stylesheet.create({
+const styles = StyleSheet.create({
     container: {
-        flex:1,
-        backgroundColor:'#000',
+        flex: 1,
+        backgroundColor: '#000',
         alignItems: 'center',
         justifyContent: 'center',
-
-    },   
+    },
     textInput: {
-        paddingTop:40,
-        paddingBottom:20,
-        paddingHorizontal:20,
-        fontSize:24,
+        paddingTop: 40,
+        paddingBottom: 20,
+        paddingHorizontal: 20,
+        fontSize: 24,
         borderBottomColor: '#fff',
         borderBottomWidth: 2,
-        marginBottom:20,
+        marginBottom: 20,
         textAlign: 'center',
-        color:'#fff'
+        color: '#fff',
     },
-    sendVerification:{
-        padding:20,
-        backgroundColor:'#3498db',
-        borderRadius:10,
-    },
-    sendCode:{
-        padding:20,
-        backgroundColor:'#9b59',
-        borderRadius:10,
-
-    },
-    buttonText:{
-        padding:20,
-        backgroundColor:'#9b59b6',
-        borderRadius:10,
+    sendVerification: {
+        padding: 20,
+        backgroundColor: '#3498db',
+        borderRadius: 10,
+        marginBottom: 20,
     },
     buttonText: {
-        textAlign:'center',
-        color:'#fff',
-        fontWeight:'bold',
+        textAlign: 'center',
+        color: '#fff',
+        fontWeight: 'bold',
     },
     otpText: {
-        fontSize:24,
-        fontWeight:'bold',
-        color:'#fff',
-        margin:20
-    }
-
-})
-
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: '#fff',
+        margin: 20,
+    },
+});
